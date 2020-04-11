0dd53a7f59828f9e73a067a8824301e2d3bb467e
Maribel <maribegress@gmail.com>
2020-04-11 15:11:16 -0700

    Planes
    Using Planes and GameObjects with Vector3

---
+++ Unity Practice/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+	public Vector3 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//Vecotr3(x,y,z)= Vector3(0.1,0,0) give me a vector that is 0.01 units in size 
+      gameObject.transform.Translate(speed); 
+    }
+}
