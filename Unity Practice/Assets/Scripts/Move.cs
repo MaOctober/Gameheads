11afbaf127a95a6dbb9462f23f824572149e4687
Maribel <maribegress@gmail.com>
2020-04-14 19:16:03 -0700

    C# Moving Horizontally and Vertically
    Changes to make capsule move left right up and down.

--- Unity Practice/Assets/Scripts/Move.cs
+++ Unity Practice/Assets/Scripts/Move.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
-{
-	public Vector3 speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Move : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-    	//Vecotr3(x,y,z)= Vector3(0.1,0,0) give me a vector that is 0.01 units in size 
-      gameObject.transform.Translate(speed); 
-    }
+  
+    void Update(){
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0f);
+       
+     
+            }
+
+
 }
+    	
+
+   
+    
