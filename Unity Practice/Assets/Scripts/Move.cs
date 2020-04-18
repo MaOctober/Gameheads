759d6320cb018338846ebdf000d38626cdb46b42
Maribel <maribegress@gmail.com>
2020-04-18 14:56:45 -0700

    Camera Follow Script
    Linear Interpelation

--- Unity Practice/Assets/Scripts/Move.cs
+++ Unity Practice/Assets/Scripts/Move.cs
@@ -2,14 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class Move : MonoBehaviour
+ {
+ 	public Vector3 speed; 
 
+ 	void Start()
+ 	{
+
+
+ 	}
   
-    void Update(){
-        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0f);
-       
+    void Update()
+    {
+    	Vector3 currentSpeed = Vector3.zero;
+
+    	if (Input.GetKey(KeyCode.A))
+    	{
+
+    		currentSpeed.x = -speed.x; // current speed.x = currentSpeed.x * -1.0f 
+
+    	}
+    	if (Input.GetKey(KeyCode.D))
+    	{
+
+    		currentSpeed.x = speed.x; // current speed.x = currentSpeed.x * -1.0f 
+
+    	}
+    	if (Input.GetKey(KeyCode.W))
+    	{
+
+    		currentSpeed.z = speed.z; // current speed.x = currentSpeed.x * -1.0f 
+
+    	}
+    	if (Input.GetKey(KeyCode.S))
+    	{
+
+    		currentSpeed.z = -speed.z; // current speed.x = currentSpeed.x * -1.0f 
+
+    	}
+        //transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0f);
+        	gameObject.transform.Translate(currentSpeed * Time.deltaTime);
      
-            }
+        }
 
 
 }
