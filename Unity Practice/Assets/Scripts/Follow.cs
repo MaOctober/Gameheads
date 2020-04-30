213dd4a532f39860170b42d000b98a815c408ac5
Maribel <maribegress@gmail.com>
2020-04-30 13:44:25 -0700

    Jump
    Camera position / rigidbody collision

--- Unity Practice/Assets/Scripts/Follow.cs
+++ Unity Practice/Assets/Scripts/Follow.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
-		public GameObject target; 
-		public float followDistance;
-		public float cameraFollowSpeed = 0.1f;
-	
+    public GameObject target;
+    public float followDistance;
+    public Vector3 followOffset;
+    public float cameraFollowSpeed = 0.1f;
+    public float cameraRotationSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 targetPosition = target.transform.position;  
+        Vector3 targetPosition = target.transform.position;
 
-      	Vector3 followVector = transform.forward * -followDistance;
+        Vector3 followVector = transform.forward * -followDistance;
 
-        transform.position = targetPosition + followVector; 
+        transform.position = targetPosition + followVector + followOffset;
 
+        transform.rotation = Quaternion.LookRotation(target.transform.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-   
-      Vector3 targetPosition = target.transform.position;  
+        Vector3 targetPosition = target.transform.position;
 
-      Vector3 followVector = transform.forward * -followDistance;
+        Vector3 followVector = transform.forward * -followDistance;
 
-      Vector3 cameraTargetPosition = targetPosition + followVector; 
+        Vector3 cameraTargetPosition = targetPosition + followVector + followOffset;
 
-      transform.position = Vector3.Lerp(transform.position, cameraTargetPosition,
-       cameraFollowSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, cameraTargetPosition, 
+            cameraFollowSpeed * Time.deltaTime);
+        
+        //Which way is the capsule facing - target.transform.forward
+        Vector3 targetForward = target.transform.forward;
 
-     
+        //Taking the camera direction and slowly rotating towards the capsule forward
+        Vector3 direction = Vector3.RotateTowards(transform.forward, targetForward,
+            cameraRotationSpeed * Time.deltaTime, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(direction);
     }
+
+    //Vector3 cameraTargetPosition = CalculateCameraTargetPosition();
+    /*
+    Vector3 CalculateCameraTargetPositionAndRotation()
+    {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 followVector = transform.forward * -followDistance;
+        Vector3 cameraTargetPosition = targetPosition + followVector;
+        return cameraTargetPosition;
+    }*/
 }
--- Unity Practice/Assets/Scripts/Move.cs
+++ Unity Practice/Assets/Scripts/Move.cs
@@ -3,55 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
- {
- 	public Vector3 speed; 
-    public Vector3 target = new Vector3(5.0f, 0.0f, 0.0f);
+{
+    public Vector3 speed; //Amount of units to move per second
+    
+    //Amount of degrees per second to turn
+    public float turnSpeed;
+    public float jumpforce = 1.0f; 
 
- 	void Start()
- 	{
+    private bool isJumping = false;
+    private Rigidbody rb; 
+    private bool onGround = false;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
- 	}
-  
+    // Update is called once per frame
+    /*void Update()
+    {
+        Vector3 currentSpeed = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentSpeed.x = -speed.x; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentSpeed.x = speed.x;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed.z = speed.z;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+        //Speed is a Vector3 defines how fast to move in 3D space
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
+    }*/
+
     void Update()
     {
+        
+    }
+    private void FixedUpdate()
+    {
 
-    	Vector3 currentSpeed = Vector3.zero;
+float currentSpeed = 0.0f;
+        float currentTurnAmount = 0.0f;
 
-    	if (Input.GetKey(KeyCode.A))
-    	{
-            transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
-
-    		//currentSpeed.x = -speed.x; // current speed.x = currentSpeed.x * -1.0f 
-            
-    	}
-    	if (Input.GetKey(KeyCode.D))
-    	{
-
-    		currentSpeed.x = speed.x; // current speed.x = currentSpeed.x * -1.0f 
-
-    	}
-    	if (Input.GetKey(KeyCode.W))
-    	{
-
-    		currentSpeed.z = speed.z; // current speed.x = currentSpeed.x * -1.0f 
-
-    	}
-    	if (Input.GetKey(KeyCode.S))
-    	{
-
-    		currentSpeed.z = -speed.z; // current speed.x = currentSpeed.x * -1.0f 
-
-    	}
-        //transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Vertical")*Time.deltaTime, 0f);
-            gameObject.transform.Translate(currentSpeed * Time.deltaTime);
-            //transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
-     
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentTurnAmount -= turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentTurnAmount += turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = speed.x;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentSpeed = -speed.x;
         }
 
 
-}
-    	
+        //Speed is a Vector3 defines how fast to move in 3D space
+        gameObject.transform.Rotate(Vector3.up, currentTurnAmount * Time.deltaTime);
+        rb.AddForce(transform.forward * currentSpeed * Time.deltaTime, ForceMode.Impulse);
 
-   
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isJumping = true;
+        }
+
+        rb.angularVelocity = Vector3.zero;
+    }
+
     
+}
