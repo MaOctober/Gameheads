3b45494f9f950414be7ee72dd8535f4b8cbe5f36
Maribel <maribegress@gmail.com>
2020-05-23 11:56:45 -0700

    Text Mesh Pro
    failed to update score

--- Unity Practice/Assets/Scripts/PickUp.cs
+++ Unity Practice/Assets/Scripts/PickUp.cs
@@ -9,14 +9,9 @@ public class PickUp : MonoBehaviour
     private float coin = 0;
 
 
-    public TextMeshProUGUI textCoins;
-
-
-
     private void OnTriggerEnter2D(Collider2D other){
     	if(other.gameObject.CompareTag("Coin")){
     		coin ++;
-    		textCoins.text = coin.ToString()
 
     		Destroy(other.gameObject);
     	}
